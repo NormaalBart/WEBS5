f1b600a1d39a9088a637d77512e71fce6dae9ee4
Bart <bwj.kempen@student.avans.nl>
2024-04-03 00:18:29 +0200

    Update app.js

--- apigateway/app.js
+++ apigateway/app.js
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 3000
 
 registerRoutes(app)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swa  ggerUi.setup(swaggerDocument))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
